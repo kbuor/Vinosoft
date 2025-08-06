defc9960b740ce615e6ea9147cd4831db08aef5a
kbuor <77895173+kbuor@users.noreply.github.com>
2025-08-06 23:12:41 +0700

    Update and rename k8s-alma9-initial-setup.bash to k8s-alma9-prepare-node.bash

---
+++ k8s-alma9-prepare-node.bash
@@ -0,0 +1,58 @@
+#!/bin/bash
+dnf update -y
+dns install -y wget python3-dnf-plugins-extras-versionlock
+swapoff -a
+sed -i '/ swap / s/^/#/' /etc/fstab
+modprobe overlay
+modprobe br_netfilter
+tee /etc/modules-load.d/containerd.conf <<'EOF'
+overlay
+br_netfilter
+EOF
+
+tee /etc/sysctl.d/99-kubernetes-cri.conf <<'EOF'
+net.bridge.bridge-nf-call-iptables = 1
+net.ipv4.ip_forward = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+EOF
+
+sysctl --system
+wget https://github.com/containerd/containerd/releases/download/v1.7.13/containerd-1.7.13-linux-amd64.tar.gz -P /tmp/
+tar -C /usr/local -xzf /tmp/containerd-1.7.13-linux-amd64.tar.gz
+
+wget https://raw.githubusercontent.com/containerd/containerd/main/containerd.service -P /etc/systemd/system/
+systemctl daemon-reload
+systemctl enable --now containerd
+
+wget https://github.com/opencontainers/runc/releases/download/v1.1.12/runc.amd64 -P /tmp/
+install -m 755 /tmp/runc.amd64 /usr/local/sbin/runc
+
+wget https://github.com/containernetworking/plugins/releases/download/v1.4.0/cni-plugins-linux-amd64-v1.4.0.tgz -P /tmp/
+mkdir -p /opt/cni/bin
+tar -C /opt/cni/bin -xzf /tmp/cni-plugins-linux-amd64-v1.4.0.tgz
+
+mkdir -p /etc/containerd
+containerd config default | tee /etc/containerd/config.toml
+
+####
+####
+
+dnf install -y curl wget vim git bash-completion lvm2 device-mapper-persistent-data
+cat <<'EOF' > /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://pkgs.k8s.io/core:/stable:/v1.29/rpm/
+enabled=1
+gpgcheck=1
+gpgkey=https://pkgs.k8s.io/core:/stable:/v1.29/rpm/repodata/repomd.xml.key
+EOF
+
+setenforce 0
+sed -i --follow-symlinks 's/^SELINUX=enforcing$/SELINUX=permissive/' /etc/selinux/config
+
+dnf install -y kubelet-1.29.1 kubeadm-1.29.1 kubectl-1.29.1
+
+dnf versionlock add kubelet kubeadm kubectl
+
+systemctl enable --now kubelet
+
