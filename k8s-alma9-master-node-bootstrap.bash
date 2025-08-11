cc1204b45bdd5d394f577fe2e48f807c3761a972
kbuor <77895173+kbuor@users.noreply.github.com>
2025-08-11 14:51:05 +0700

    Update k8s-alma9-master-node-bootstrap.bash

--- k8s-alma9-master-node-bootstrap.bash
+++ k8s-alma9-master-node-bootstrap.bash
@@ -1,20 +1,26 @@
 #!/bin/bash
-#Bootstrap Master
+# Bootstrap Master
 kubeadm init --pod-network-cidr=192.168.0.0/16 --node-name=$NODE_NAME --apiserver-cert-extra-sans=$IP_LOCAL --apiserver-cert-extra-sans=$NODE_NAME
 
-#Configure Credentials
+# Configure Credentials
 mkdir -p $HOME/.kube
 cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 chown $(id -u):$(id -g) $HOME/.kube/config
 
-#Deploy Calico Network
+# Deploy Calico Network
 kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.30.2/manifests/tigera-operator.yaml
+sleep 10
 kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.30.2/manifests/custom-resources.yaml
+sleep 10
 
 # Deploy Nginx-Ingress
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/refs/heads/main/config/manifests/metallb-native.yaml
+sleep 10
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/main/config/crd/bases/metallb.io_ipaddresspools.yaml
+sleep 10
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/main/config/crd/bases/metallb.io_l2advertisements.yaml
+sleep 10
+
 cat <'EOF' > /tmp/metallb-config.yaml
 apiVersion: metallb.io/v1beta1
 kind: IPAddressPool
@@ -23,7 +29,7 @@ metadata:
   namespace: metallb-system
 spec:
   addresses:
-  - 10.20.30.200-10.20.30.210  # Dải IP được chọn
+  - 10.0.1.201-10.0.1.209  # Node Network
 ---
 apiVersion: metallb.io/v1beta1
 kind: L2Advertisement
@@ -32,3 +38,17 @@ metadata:
   namespace: metallb-system
 spec: {}
 EOF
+
+kubectl apply -f /tmp/metallb-config.yaml
+sleep 10
+
+# Install Helm Chart
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+chmod 700 get_helm.sh
+./get_helm.sh
+
+# Install Nginx Ingress
+helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
+helm repo update
+helm install ingress-nginx ingress-nginx/ingress-nginx --namespace ingress-nginx --create-namespace
+sleep 10
